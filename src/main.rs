f3af6d7600bc5e6fa5ec64a89afd8e27d0136d76
James Hiew <james@hiew.net>
2017-11-21 00:50:00 +0000

    Copy over teapot example from glium repository

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,170 @@
-extern crate gl;
-extern crate glutin;
-extern crate libc;
+#[macro_use]
+extern crate glium;
 
-use glutin::GlContext;
+use glium::{glutin, Surface};
+
+mod support;
 
 fn main() {
+    // building the display, ie. the main object
     let mut events_loop = glutin::EventsLoop::new();
-    let window = glutin::WindowBuilder::new()
-        .with_title("Hello, world!")
-        .with_dimensions(1024, 768);
-    let context = glutin::ContextBuilder::new()
-        .with_vsync(true);
-    let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
+    let window = glutin::WindowBuilder::new();
+    let context = glutin::ContextBuilder::new().with_depth_buffer(24);
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
 
-    unsafe {
-        gl_window.make_current().unwrap();
-    }
+    // building the vertex and index buffers
+    let vertex_buffer = support::load_wavefront(&display, include_bytes!("support/teapot.obj"));
 
-    unsafe {
-        gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
-        gl::ClearColor(0.0, 1.0, 0.0, 1.0);
-    }
+    // the program
+    let program = program!(&display,
+        140 => {
+            vertex: "
+                #version 140
 
-    let mut running = true;
-    while running {
+                uniform mat4 persp_matrix;
+                uniform mat4 view_matrix;
+
+                in vec3 position;
+                in vec3 normal;
+                out vec3 v_position;
+                out vec3 v_normal;
+
+                void main() {
+                    v_position = position;
+                    v_normal = normal;
+                    gl_Position = persp_matrix * view_matrix * vec4(v_position * 0.005, 1.0);
+                }
+            ",
+
+            fragment: "
+                #version 140
+
+                in vec3 v_normal;
+                out vec4 f_color;
+
+                const vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
+
+                void main() {
+                    float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
+                    vec3 color = (0.3 + 0.7 * lum) * vec3(1.0, 1.0, 1.0);
+                    f_color = vec4(color, 1.0);
+                }
+            ",
+        },
+
+        110 => {
+            vertex: "
+                #version 110
+
+                uniform mat4 persp_matrix;
+                uniform mat4 view_matrix;
+
+                attribute vec3 position;
+                attribute vec3 normal;
+                varying vec3 v_position;
+                varying vec3 v_normal;
+
+                void main() {
+                    v_position = position;
+                    v_normal = normal;
+                    gl_Position = persp_matrix * view_matrix * vec4(v_position * 0.005, 1.0);
+                }
+            ",
+
+            fragment: "
+                #version 110
+
+                varying vec3 v_normal;
+
+                const vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
+
+                void main() {
+                    float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
+                    vec3 color = (0.3 + 0.7 * lum) * vec3(1.0, 1.0, 1.0);
+                    gl_FragColor = vec4(color, 1.0);
+                }
+            ",
+        },
+
+        100 => {
+            vertex: "
+                #version 100
+
+                uniform lowp mat4 persp_matrix;
+                uniform lowp mat4 view_matrix;
+
+                attribute lowp vec3 position;
+                attribute lowp vec3 normal;
+                varying lowp vec3 v_position;
+                varying lowp vec3 v_normal;
+
+                void main() {
+                    v_position = position;
+                    v_normal = normal;
+                    gl_Position = persp_matrix * view_matrix * vec4(v_position * 0.005, 1.0);
+                }
+            ",
+
+            fragment: "
+                #version 100
+
+                varying lowp vec3 v_normal;
+
+                const lowp vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
+
+                void main() {
+                    lowp float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
+                    lowp vec3 color = (0.3 + 0.7 * lum) * vec3(1.0, 1.0, 1.0);
+                    gl_FragColor = vec4(color, 1.0);
+                }
+            ",
+        },
+    ).unwrap();
+
+    //
+    let mut camera = support::camera::CameraState::new();
+
+    // the main loop
+    support::start_loop(|| {
+        camera.update();
+
+        // building the uniforms
+        let uniforms = uniform! {
+            persp_matrix: camera.get_perspective(),
+            view_matrix: camera.get_view(),
+        };
+
+        // draw parameters
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::DepthTest::IfLess,
+                write: true,
+                .. Default::default()
+            },
+            .. Default::default()
+        };
+
+        // drawing a frame
+        let mut target = display.draw();
+        target.clear_color_and_depth((0.0, 0.0, 0.0, 0.0), 1.0);
+        target.draw(&vertex_buffer,
+                    &glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList),
+                    &program, &uniforms, &params).unwrap();
+        target.finish().unwrap();
+
+        let mut action = support::Action::Continue;
+
+        // polling and handling the events received by the window
         events_loop.poll_events(|event| {
             match event {
-                glutin::Event::WindowEvent{ event, .. } => match event {
-                    glutin::WindowEvent::Closed => running = false,
-                    glutin::WindowEvent::Resized(w, h) => gl_window.resize(w, h),
-                    _ => ()
+                glutin::Event::WindowEvent { event, .. } => match event {
+                    glutin::WindowEvent::Closed => action = support::Action::Stop,
+                    ev => camera.process_input(&ev),
                 },
-                _ => ()
+                _ => (),
             }
         });
 
-        unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-        }
-
-        gl_window.swap_buffers().unwrap();
-    }
+        action
+    });
 }
---
+++ src/support/mod.rs
@@ -0,0 +1,84 @@
+#![allow(dead_code)]
+
+extern crate genmesh;
+extern crate obj;
+
+use std::thread;
+use std::time::{Duration, Instant};
+use glium::{self, Display};
+use glium::vertex::VertexBufferAny;
+
+pub mod camera;
+
+pub enum Action {
+    Stop,
+    Continue,
+}
+
+pub fn start_loop<F>(mut callback: F) where F: FnMut() -> Action {
+    let mut accumulator = Duration::new(0, 0);
+    let mut previous_clock = Instant::now();
+
+    loop {
+        match callback() {
+            Action::Stop => break,
+            Action::Continue => ()
+        };
+
+        let now = Instant::now();
+        accumulator += now - previous_clock;
+        previous_clock = now;
+
+        let fixed_time_stamp = Duration::new(0, 16666667);
+        while accumulator >= fixed_time_stamp {
+            accumulator -= fixed_time_stamp;
+
+            // if you have a game, update the state here
+        }
+
+        thread::sleep(fixed_time_stamp - accumulator);
+    }
+}
+
+/// Returns a vertex buffer that should be rendered as `TrianglesList`.
+pub fn load_wavefront(display: &Display, data: &[u8]) -> VertexBufferAny {
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 3],
+        normal: [f32; 3],
+        texture: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position, normal, texture);
+
+    let mut data = ::std::io::BufReader::new(data);
+    let data = obj::Obj::load_buf(&mut data).unwrap();
+
+    let mut vertex_data = Vec::new();
+
+    for object in data.objects.iter() {
+        for polygon in object.groups.iter().flat_map(|g| g.polys.iter()) {
+            match polygon {
+                &genmesh::Polygon::PolyTri(genmesh::Triangle { x: v1, y: v2, z: v3 }) => {
+                    for v in [v1, v2, v3].iter() {
+                        let position = data.position[v.0];
+                        let texture = v.1.map(|index| data.texture[index]);
+                        let normal = v.2.map(|index| data.normal[index]);
+
+                        let texture = texture.unwrap_or([0.0, 0.0]);
+                        let normal = normal.unwrap_or([0.0, 0.0, 0.0]);
+
+                        vertex_data.push(Vertex {
+                            position: position,
+                            normal: normal,
+                            texture: texture,
+                        })
+                    }
+                },
+                _ => unimplemented!()
+            }
+        }
+    }
+
+    glium::vertex::VertexBuffer::new(display, &vertex_data).unwrap().into_vertex_buffer_any()
+}
