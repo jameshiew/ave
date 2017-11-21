f969726dcf4a438839e920b15a978fc6212b7753
James Hiew <james@hiew.net>
2017-11-21 02:21:43 +0000

    Make blocks actual cubes rather than rectangles

--- src/block.rs
+++ src/block.rs
@@ -13,15 +13,40 @@ pub struct Block {
 
 impl Block {
     pub fn new(x: f32, y: f32, z: f32) -> Block {
-        Block { x, y, z, }
+        Block { x, y, z }
     }
 
-    pub fn get_vertices<F: ?Sized>(self, facade: &F) -> VertexBuffer<Vertex> where F: Facade {
+    pub fn get_vertices<F: ? Sized>(self, facade: &F) -> VertexBuffer<Vertex> where F: Facade {
         return VertexBuffer::new(facade, &[
-            Vertex { position: [self.x - 1.0, self.y + 1.0, self.z], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 1.0] },
-            Vertex { position: [self.x + 1.0, self.y + 1.0, self.z], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 1.0] },
-            Vertex { position: [self.x - 1.0, self.y - 1.0, self.z], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 0.0] },
-            Vertex { position: [self.x + 1.0, self.y - 1.0, self.z], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 0.0] },
+            Vertex { position: [self.x - 1.0, self.y - 1.0, self.z - 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 1.0]},
+            Vertex { position: [self.x - 1.0, self.y - 1.0, self.z + 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 1.0]},
+            Vertex { position: [self.x - 1.0, self.y + 1.0, self.z - 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 0.0]},
+            Vertex { position: [self.x - 1.0, self.y + 1.0, self.z + 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 0.0]},
+
+            Vertex { position: [self.x + 1.0, self.y - 1.0, self.z - 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 1.0]},
+            Vertex { position: [self.x + 1.0, self.y - 1.0, self.z + 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 1.0]},
+            Vertex { position: [self.x + 1.0, self.y + 1.0, self.z - 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 0.0]},
+            Vertex { position: [self.x + 1.0, self.y + 1.0, self.z + 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 0.0]},
+
+            Vertex { position: [self.x - 1.0, self.y + 1.0, self.z - 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 1.0]},
+            Vertex { position: [self.x - 1.0, self.y + 1.0, self.z + 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 1.0]},
+            Vertex { position: [self.x + 1.0, self.y + 1.0, self.z - 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 0.0]},
+            Vertex { position: [self.x + 1.0, self.y + 1.0, self.z + 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 0.0]},
+
+            Vertex { position: [self.x - 1.0, self.y - 1.0, self.z - 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 1.0]},
+            Vertex { position: [self.x - 1.0, self.y - 1.0, self.z + 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 1.0]},
+            Vertex { position: [self.x + 1.0, self.y - 1.0, self.z - 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 0.0]},
+            Vertex { position: [self.x + 1.0, self.y - 1.0, self.z + 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 0.0]},
+
+            Vertex { position: [self.x - 1.0, self.y - 1.0, self.z - 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 1.0]},
+            Vertex { position: [self.x - 1.0, self.y + 1.0, self.z - 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 1.0]},
+            Vertex { position: [self.x + 1.0, self.y - 1.0, self.z - 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 0.0]},
+            Vertex { position: [self.x + 1.0, self.y + 1.0, self.z - 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 0.0]},
+
+            Vertex { position: [self.x - 1.0, self.y - 1.0, self.z + 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 1.0]},
+            Vertex { position: [self.x - 1.0, self.y + 1.0, self.z + 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 1.0]},
+            Vertex { position: [self.x + 1.0, self.y - 1.0, self.z + 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [0.0, 0.0]},
+            Vertex { position: [self.x + 1.0, self.y + 1.0, self.z + 1.0], normal: [0.0, 0.0, self.z - 1.0], tex_coords: [1.0, 0.0]}
         ]).unwrap();
     }
 }
