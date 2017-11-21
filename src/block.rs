c48ad4c3ad5ecc92de1dea1b7555e83c57c063ce
James Hiew <james@hiew.net>
2017-11-21 01:43:13 +0000

    Stub out a Block struct

---
+++ src/block.rs
@@ -0,0 +1,15 @@
+pub struct Block {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Block {
+    pub fn new() -> Block {
+        Block {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }
+    }
+}
