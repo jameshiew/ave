b48fe5cc703520bb3822d0b88eba97761b00b0e9
James Hiew <james@hiew.net>
2017-11-23 21:50:58 +0000

    Implement Debug, Eq, PartialEq for Position and Direction types

--- src/space.rs
+++ src/space.rs
@@ -1,2 +1,23 @@
+extern crate float_cmp;
+
+use self::float_cmp::ApproxEqUlps;
+
+#[derive(Debug)]
 pub struct Position(pub f32, pub f32, pub f32);
+
+impl PartialEq for Position {
+    fn eq(&self, other: &Position) -> bool {
+        self.0.approx_eq_ulps(&other.0, 2) && self.1.approx_eq_ulps(&other.1, 2) && self.2.approx_eq_ulps(&other.2, 2)
+    }
+}
+impl Eq for Position {}
+
+#[derive(Debug)]
 pub struct Direction(pub f32, pub f32, pub f32);
+
+impl PartialEq for Direction {
+    fn eq(&self, other: &Direction) -> bool {
+        self.0.approx_eq_ulps(&other.0, 2) && self.1.approx_eq_ulps(&other.1, 2) && self.2.approx_eq_ulps(&other.2, 2)
+    }
+}
+impl Eq for Direction {}
