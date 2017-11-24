b5a61f221700007464da1eb3c7e89ec1fe28b225
James Hiew <james@hiew.net>
2017-11-24 18:46:02 +0000

    Create Adjacent trait

--- src/space.rs
+++ src/space.rs
@@ -1,4 +1,12 @@
 use cgmath::Vector3;
+use std::vec::Vec;
+use std;
 
 pub type Position = Vector3<f32>;
 pub type Direction = Vector3<f32>;
+
+pub trait Adjacent {
+    fn adjacent(&self) -> Vec<Self> where Self: std::marker::Sized;
+    fn directly_adjacent(&self) -> Vec<Self> where Self: std::marker::Sized;
+    fn diagonally_adjacent(&self) -> Vec<Self> where Self: std::marker::Sized;
+}
