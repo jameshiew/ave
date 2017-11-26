3df4a770c96d22d870dca5c53cb2a23c28cbf5ee
James Hiew <james@hiew.net>
2017-11-26 23:19:32 +0000

    Remove all dead code from space.rs

--- src/space.rs
+++ src/space.rs
@@ -1,5 +1,4 @@
 use cgmath::{Vector3, Point3};
-use collision::Plane;
 use std::vec::Vec;
 use std;
 
@@ -24,25 +23,4 @@ pub trait Adjacent {
     fn adjacent(&self) -> Vec<Self> where Self: std::marker::Sized;
     fn directly_adjacent(&self) -> Vec<Self> where Self: std::marker::Sized;
     fn diagonally_adjacent(&self) -> Vec<Self> where Self: std::marker::Sized;
-}
-
-
-/// See https://www.khronos.org/opengl/wiki/Calculating_a_Surface_Normal
-pub fn get_normal_for_triangle(point_a: Position, point_b: Position, point_c: Position) -> Direction {
-    Plane::from_points(point_a, point_b, point_c).unwrap().n
-}
-
-
-#[cfg(test)]
-mod tests {
-    use space::get_normal_for_triangle;
-
-//    #[test]
-//    fn test_get_normal_for_triangle() {
-//        assert_ne!(get_normal_for_triangle(
-//            [0.0, 0.0, 0.0].into(),
-//            [1.0, 0.0, 0.0].into(),
-//            [0.0, 3.0, 0.0].into(),
-//        ),
-//    }
 }
