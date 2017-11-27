050680e0744e4377504075f5a6b980456fe4b8b4
James Hiew <james@hiew.net>
2017-11-27 12:40:46 +0000

    Reduce default render distance to 2 chunks again

--- src/default.rs
+++ src/default.rs
@@ -1,7 +1,7 @@
 /// Default settings go here
 
-pub const RENDER_DISTANCE_U8: u8 = 3;
-pub const RENDER_DISTANCE_I32: i32 = 3;
+pub const RENDER_DISTANCE_U8: u8 = 2;
+pub const RENDER_DISTANCE_I32: i32 = 2;
 
 pub const VIEWPORT_WIDTH: u32 = 1024;
 pub const VIEWPORT_HEIGHT: u32 = 768;
