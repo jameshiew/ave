eabcabb2056d092710e758ac83c434b332e59dee
EternalMC <55027815+EternalMC@users.noreply.github.com>
2020-02-18 21:16:17 -0600

    Update color.rs

--- src/color.rs
+++ src/color.rs
@@ -2,10 +2,10 @@ use rand;
 use rand::Rng;
 
 pub type Color = [f32; 3];
-
+/*
 pub fn get_random_color() -> Color {
     let mut rng = rand::thread_rng();
     [rng.gen_range(0.0, 1.0), rng.gen_range(0.0, 1.0), rng.gen_range(0.0, 1.0)]
 }
-
+*/
 pub const SKY: Color = [0.529, 0.808, 0.980];
