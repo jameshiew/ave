24915602112633b93b4e4ff32259cae204b35c87
James Hiew <james@hiew.net>
2020-04-21 01:29:12 +0100

    color.rs: allow dead code for random color func

--- src/color.rs
+++ src/color.rs
@@ -3,6 +3,7 @@ use rand::Rng;
 
 pub type Color = [f32; 3];
 
+#[allow(dead_code)]
 pub fn get_random_color() -> Color {
     let mut rng = rand::thread_rng();
     [rng.gen_range(0.0, 1.0), rng.gen_range(0.0, 1.0), rng.gen_range(0.0, 1.0)]
