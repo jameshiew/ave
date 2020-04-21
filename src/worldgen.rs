4a54f7d3d75493e48e87f991003ced5a1fd78df7
James Hiew <james@hiew.net>
2020-04-21 20:10:24 +0100

    upgrade noise to 0.5.1

--- src/worldgen.rs
+++ src/worldgen.rs
@@ -1,7 +1,7 @@
 use crate::block;
 use crate::world::{get_position, Chunk, ChunkCoordinates, HashChunk, CHUNK_SIZE};
 use log::debug;
-use noise::{NoiseModule, Perlin, Seedable};
+use noise::{NoiseFn, Perlin, Seedable};
 use rand::{Rng, SeedableRng, StdRng};
 
 pub trait WorldGenerator {
@@ -83,14 +83,14 @@ pub struct NaturalWorldGenerator {
 }
 
 impl NaturalWorldGenerator {
-    pub fn new(seed: usize) -> NaturalWorldGenerator {
+    pub fn new(seed: u32) -> NaturalWorldGenerator {
         let mut generator = NaturalWorldGenerator {
             perlin: Perlin::new(),
         };
         generator.perlin = generator.perlin.set_seed(seed);
         debug!(
             "Using seed {} for NaturalWorldGenerator",
-            generator.perlin.seed
+            generator.perlin.seed()
         );
         generator
     }
@@ -106,9 +106,9 @@ impl WorldGenerator for NaturalWorldGenerator {
                     // we need a height in the range [0, CHUNK_SIZE)
                     // https://www.redblobgames.com/maps/terrain-from-noise/ is a good source for tips
                     let position = get_position(&coordinates, [x, 0, z].into());
-                    let height = self.perlin.get([position.x * 0.015, position.z * 0.015]);
+                    let height = self.perlin.get([(position.x * 0.015) as f64, (position.z * 0.015) as f64]);
                     // raise height to decent even power to so we get more flats and its nonnegative
-                    let normalized_height: u8 = (height.powi(4) * (CHUNK_SIZE as f32)) as u8;
+                    let normalized_height: u8 = (height.powi(4) * (CHUNK_SIZE as f64)) as u8;
                     let mut blk = block::GRASS;
                     if normalized_height == 0 {
                         if height < 0.0 {
