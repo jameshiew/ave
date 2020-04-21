f7a992a6f84e99e6724221244369337edab83406
James Hiew <james@hiew.net>
2020-04-21 15:40:38 +0100

    rustfmt

--- src/color.rs
+++ src/color.rs
@@ -6,7 +6,11 @@ pub type Color = [f32; 3];
 #[allow(dead_code)]
 pub fn get_random_color() -> Color {
     let mut rng = rand::thread_rng();
-    [rng.gen_range(0.0, 1.0), rng.gen_range(0.0, 1.0), rng.gen_range(0.0, 1.0)]
+    [
+        rng.gen_range(0.0, 1.0),
+        rng.gen_range(0.0, 1.0),
+        rng.gen_range(0.0, 1.0),
+    ]
 }
 
 pub const SKY: Color = [0.529, 0.808, 0.980];
